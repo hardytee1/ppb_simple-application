5759fff0a49af44a6ecc44fdd661f6356a7ac4e9
hardytee1 <143975733+hardytee1@users.noreply.github.com>
2025-03-24 10:20:59 +0700

    add the base for instagram app

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,200 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+}
+
+class Post {
+  int id;
+  String title;
+  String caption;
+  String imageUrl;
+
+  Post({
+    required this.id,
+    required this.title,
+    required this.caption,
+    required this.imageUrl,
+  });
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Instagram',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: PostListPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class PostListPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _PostListPageState createState() => _PostListPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _PostListPageState extends State<PostListPage> {
+  List<Post> posts = [
+    Post(
+      id: 1,
+      title: 'Post 1',
+      caption: 'Amazing scenery!',
+      imageUrl:
+      'https://cdn0-production-images-kly.akamaized.net/ipCXTzX3F9ojmAGeMmrNRwTxbIc=/640x360/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/3525017/original/020979200_1627547148-theleiofficial.jpeg',
+    ),
+    Post(
+      id: 2,
+      title: 'Post 2',
+      caption: 'Another great view.',
+      imageUrl:
+      'https://cdn0-production-images-kly.akamaized.net/ipCXTzX3F9ojmAGeMmrNRwTxbIc=/640x360/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/3525017/original/020979200_1627547148-theleiofficial.jpeg',
+    ),
+    Post(
+      id: 3,
+      title: 'Post 3',
+      caption: 'Enjoying the moment!',
+      imageUrl:
+      'https://cdn0-production-images-kly.akamaized.net/ipCXTzX3F9ojmAGeMmrNRwTxbIc=/640x360/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/3525017/original/020979200_1627547148-theleiofficial.jpeg',
+    ),
+  ];
 
-  void _incrementCounter() {
+  int _nextId = 4;
+
+  void _addOrEditPost({Post? post}) {
+    final isEditing = post != null;
+    final titleController = TextEditingController(text: post?.title ?? '');
+    final captionController = TextEditingController(text: post?.caption ?? '');
+    final imageUrlController =
+    TextEditingController(text: post?.imageUrl ?? '');
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(isEditing ? 'Edit Post' : 'New Post'),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: titleController,
+                  decoration: InputDecoration(labelText: 'Title'),
+                ),
+                TextField(
+                  controller: captionController,
+                  decoration: InputDecoration(labelText: 'Caption'),
+                ),
+                TextField(
+                  controller: imageUrlController,
+                  decoration: InputDecoration(labelText: 'Image URL'),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            ElevatedButton(
+              child: Text(isEditing ? 'Save' : 'Add'),
+              onPressed: () {
+                final title = titleController.text;
+                final caption = captionController.text;
+                final imageUrl = imageUrlController.text;
+                if (title.isNotEmpty &&
+                    caption.isNotEmpty &&
+                    imageUrl.isNotEmpty) {
+                  setState(() {
+                    if (isEditing) {
+                      post!.title = title;
+                      post.caption = caption;
+                      post.imageUrl = imageUrl;
+                    } else {
+                      posts.add(Post(
+                          id: _nextId++,
+                          title: title,
+                          caption: caption,
+                          imageUrl: imageUrl));
+                    }
+                  });
+                  Navigator.of(context).pop();
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _deletePost(Post post) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      posts.removeWhere((p) => p.id == post.id);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Instagram'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: ListView.builder(
+        itemCount: posts.length,
+        itemBuilder: (context, index) {
+          final post = posts[index];
+          return Card(
+            margin: EdgeInsets.all(8.0),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    post.title,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Text(post.caption),
+                  SizedBox(height: 8),
+                  Image.network(
+                    post.imageUrl,
+                    errorBuilder: (context, error, stackTrace) {
+                      return Text('Error loading image');
+                    },
+                  ),
+                  ButtonBar(
+                    alignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.edit),
+                        onPressed: () => _addOrEditPost(post: post),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () => _deletePost(post),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () => _addOrEditPost(),
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
